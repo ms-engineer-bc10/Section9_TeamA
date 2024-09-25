6e410872dc54508dc1e81d133073a9ecb93afe21
masakohayakawa <hayakawam01@gmail.com>
2024-09-26 00:17:56 +0900

    firstmigration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,42 +1,9 @@
-# import os
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-
-# # アプリケーションファクトリ関数を定義
-# def create_app():
-#     app = Flask(__name__)
-
-# # データベース設定
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@db/souvenir_app'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# # インスタンス作成をアプリケーションファクトリ内に移動
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
-#   return app
-
-# # アプリケーションインスタンスを作成
-# app = create_app()
-
-# # モデルのインポート
-# from app.models import models
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.models import db
 
-db = SQLAlchemy()
 migrate = Migrate()
 
 # アプリケーションファクトリ関数を定義
@@ -60,9 +27,6 @@ def create_app():
 
 # アプリケーションインスタンスを作成
 app = create_app()
-
-# モデルのインポート（循環インポートを避けるため、ここで行う）
-from app.models import models
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ backend/app/models.py
@@ -0,0 +1,69 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class ApiRequest(db.Model):
+    __tablename__ = 'api_request'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(255), db.ForeignKey('user.uid'))
+    requested_at = db.Column(db.DateTime, default=datetime.utcnow)
+    api_name = db.Column(db.String(255))
+    result = db.Column(db.String(255))
+    error_message = db.Column(db.String(255))
+
+class User(db.Model):
+    __tablename__ = 'user'
+    
+    uid = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(50))
+    registered_at = db.Column(db.DateTime, default=datetime.now)
+    latest_login_at = db.Column(db.DateTime)
+    condition_count = db.Column(db.Integer, default=0)
+    recomend_count = db.Column(db.Integer, default=0)
+    stripe_auth = db.Column(db.String(255))
+    payment_reg_at = db.Column(db.DateTime)
+
+    api_requests = db.relationship('ApiRequest', backref='user', lazy=True)
+    conditions = db.relationship('Condition', backref='user', lazy=True)
+
+class Condition(db.Model):
+    __tablename__ = 'condition'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(255), db.ForeignKey('user.uid'))
+    target = db.Column(db.String(255))
+    genre = db.Column(db.String(255))
+    budget = db.Column(db.Integer)
+    quantity = db.Column(db.String(50))
+    searched_at = db.Column(db.DateTime, default=datetime.now)
+
+    recommendations = db.relationship('Recommend', backref='condition', lazy=True)
+
+class Recommend(db.Model):
+    __tablename__ = 'recommend'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    condition_id = db.Column(db.Integer, db.ForeignKey('condition.id'))
+    product_name = db.Column(db.String(255))
+    product_url = db.Column(db.String(255))
+    product_price = db.Column(db.Integer)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    recommended_at = db.Column(db.DateTime, default=datetime.now)
+
+class Store(db.Model):
+    __tablename__ = 'store'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    tel = db.Column(db.String(50))
+    open_time = db.Column(db.String(255))
+    location = db.Column(db.String(255))
+        
+    recommendations = db.relationship('Recommend', backref='store', lazy=True)
+    
