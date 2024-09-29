d1dc2e38b500a92566247cb32f896098a1b0dc80
シモカワ・アスカ <shimokawaasuka@shimokawaasukanonotobukkukonpyuta.local>
2024-09-29 14:40:17 +0900

    feat: 受取人、予算、個数、場所に基づくおみやげおすすめ機能の実装

--- backend/app/services/yahoo_service.py
+++ backend/app/services/yahoo_service.py
@@ -5,10 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("YAHOO_API_KEY")
 
-def search_yahoo_shopping(keyword):
+def search_yahoo_shopping(price_from=None, price_to=None):
     url = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
     headers = {'Authorization': f'Bearer {API_KEY}'}
-    params = {'query': keyword}
-    
+    params = {
+        'appid': 'API_KEY',
+        'sort': '+price',
+        'results': '30',
+        # "location"で入力された場所情報をqueryに含める必要あり
+        'query': 'おみやげ'
+    }
+
+    if price_from is not None:
+        params['price_from'] = price_from
+    if price_to is not None:
+        params['price_to'] = price_to
+
     response = requests.get(url, headers=headers, params=params)
     return response.json() if response.status_code == 200 else None
