b467957da98d4c05ceb77b4d7e5fb0bed91dc70d
シモカワ・アスカ <shimokawaasuka@shimokawaasukanonotobukkukonpyuta.local>
2024-10-02 16:37:46 +0900

    feat: ユーザーのリクエストに対してAPIからのデータを返す機能を実装

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['item-shopping.c.yimg.jp'],
+    },
+  };
 
 export default nextConfig;
