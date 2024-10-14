6d6efaa7e420398fa5e45bcc1871e2382a98bd33
シモカワ・アスカ <shimokawaasuka@shimokawaasukanonotobukkukonpyuta.local>
2024-10-14 14:55:02 +0900

    feat: business, likesテーブルを追加

--- backend/app/models.py
+++ backend/app/models.py
@@ -29,11 +29,13 @@ class User(db.Model):
 
     api_requests = db.relationship('ApiRequest', backref='user', lazy=True)
     conditions = db.relationship('Condition', backref='user', lazy=True)
+    businesses = db.relationship('Business', backref='user', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
 
 class Condition(db.Model):
     __tablename__ = 'condition'
     
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(255), primary_key=True)
     user_id = db.Column(db.String(255), db.ForeignKey('user.uid'))
     target = db.Column(db.String(255))
     genre = db.Column(db.String(255))
@@ -49,7 +51,7 @@ class Condition(db.Model):
 
 class Recommendation(db.Model):
     __tablename__ = 'recommendations'
-    
+
     id = db.Column(db.String(255), primary_key=True)
     condition_id = db.Column(db.String(255), db.ForeignKey('condition.id'))
     store_id = db.Column(db.String(255), db.ForeignKey('store.id'))
@@ -66,7 +68,7 @@ class Store(db.Model):
     open_time = db.Column(db.String(255))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-        
+    
     recommendations = db.relationship('Recommendation', backref='store', lazy=True)
 
 class Product(db.Model):
@@ -75,5 +77,23 @@ class Product(db.Model):
     id = db.Column(db.String(255), primary_key=True)
     name = db.Column(db.String(255))
     price = db.Column(db.Integer)
-    picture = db.Column(db.String(255))
-    comment = db.Column(db.String(255))
+    picture = db.Column(db.String(255), nullable=True)
+    comment = db.Column(db.String(255), nullable=True)
+
+class Business(db.Model):
+    __tablename__ = 'business'
+
+    id = db.Column(db.String(255), primary_key=True)
+    stripe_auth = db.Column(db.String(255))
+    payment_reg_at = db.Column(db.DateTime)
+    user_id = db.Column(db.String(255), db.ForeignKey('user.uid'))
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.String(255), primary_key=True)
+    user_id = db.Column(db.String(255), db.ForeignKey('user.uid'))
+    recommendation_id = db.Column(db.String(255), db.ForeignKey('recommendations.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    deleted_at = db.Column(db.DateTime, nullable=True)
