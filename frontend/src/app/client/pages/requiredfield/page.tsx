70148d199996e03bd01ac41474d3dee6c182858c
ki3ico <158471668+tnkKmk@users.noreply.github.com.>
2024-10-19 23:10:57 +0900

    requiredページのuseEffect削除

--- frontend/src/app/client/pages/requiredfield/page.tsx
+++ frontend/src/app/client/pages/requiredfield/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import Link from 'next/link';
 import target from '@/app/client/components/target';
 import genre from '@/app/client/components/genre';
